70c2f9188d0e017d2078acd305046e6e32a81c62
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2021-12-12 19:08:41 +0200

    create cw 12

--- JS-12_CW/class12.js
+++ JS-12_CW/class12.js
@@ -1,29 +1,32 @@
-// 1. Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+// 1. Отримати відповідь з цього ресурсу, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 // Зробити кнопку до кожного поста. При кліку на яку виводяться в окремий блок всі коментарі поточного поста
-
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(value => value.json())
-    .then(value => {
-        value.forEach(value => {
-            let divPost = document.createElement('div');
-            divPost.classList.add('divPost');
-            let userID = document.createElement('h1');
-            userID.innerText = value.userId;
-            let id = document.createElement('id');
-            id.innerText = value.id;
-            let title = document.createElement('h2');
-            title.innerText = value.title;
-            let body = document.createElement('p');
-            body.innerText = value.body;
-            let divPostButton = document.createElement('button');
-            divPostButton.innerText = 'SHOW';
-            divPost.append(userID, id, title, body, divPostButton);
-            document.body.appendChild(divPost);
+    .then(posts => {
+        posts.forEach(post => {
+                let divPost = document.createElement('div');
+                divPost.classList.add('divPost');
+                let userID = document.createElement('h1');
+                userID.innerText = post.userId;
+                let id = document.createElement('id');
+                id.innerText = post.id;
+                let title = document.createElement('h2');
+                title.innerText = post.title;
+                let body = document.createElement('p');
+                body.innerText = post.body;
+                let divPostButton = document.createElement('button');
+                divPostButton.innerText = 'SHOW COMMENT';
 
-            divPostButton.onclick = function () {
-                let details = document.createElement('div');
-
-            };
-        });
+                divPostButton.onclick = function () {
+                    let comment = document.createElement('div');
+                    comment.innerText = post;
+                    console.log(comment);
+                }
+                divPost.append(userID, id, title, body, divPostButton);
+                document.body.appendChild(divPost);
+            }
+        );
     });
+
+
