2fd26807c25c01c31dd9213f841128e149a6b3ee
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2021-12-13 23:49:39 +0200

    done cw 12

--- JS-12_CW/class12.js
+++ JS-12_CW/class12.js
@@ -1,30 +1,56 @@
 // 1. Отримати відповідь з цього ресурсу, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 // Зробити кнопку до кожного поста. При кліку на яку виводяться в окремий блок всі коментарі поточного поста
+let wrap = document.createElement('div');
+wrap.classList.add('flex')
+document.body.appendChild(wrap);
+let leftPart = document.createElement('div');
+leftPart.classList.add('left');
+wrap.appendChild(leftPart);
+const rightPart = document.createElement('div');
+wrap.appendChild(rightPart);
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(value => value.json())
     .then(posts => {
         posts.forEach(post => {
-                let divPost = document.createElement('div');
-                divPost.classList.add('divPost');
-                let userID = document.createElement('h1');
-                userID.innerText = post.userId;
-                let id = document.createElement('h3');
-                id.innerText = post.id;
-                let title = document.createElement('h2');
-                title.innerText = post.title;
-                let body = document.createElement('p');
-                body.innerText = post.body;
-                let divPostButton = document.createElement('button');
-                divPostButton.innerText = 'SHOW COMMENT';
+            let divPost = document.createElement('div');
+            divPost.classList.add('post');
+            let postUserID = document.createElement('h1');
+            postUserID.innerText = post.userId;
+            let postId = document.createElement('h3');
+            postId.innerText = post.id;
+            let postTitle = document.createElement('h2');
+            postTitle.innerText = post.title;
+            let postBody = document.createElement('p');
+            postBody.innerText = post.body;
+            let divPostButton = document.createElement('button');
+            divPostButton.innerText = 'SHOW COMMENT';
 
-                divPostButton.onclick = function () {
-                    let comment = document.createElement('div');
-                    comment.innerText = post;
-                    document.body.appendChild(comment);
-                }
-                divPost.append(userID, id, title, body, divPostButton);
-                document.body.appendChild(divPost);
-            }
-        );
+            divPostButton.onclick = function () {
+                fetch('https://jsonplaceholder.typicode.com/comments')
+                    .then(value => value.json())
+                    .then(comment => {
+                        comment.forEach(comment => {
+                            if (comment.id === post.id) {
+                                let divComment = document.createElement('div');
+                                divComment.classList.add('comment');
+                                let commentUserID = document.createElement('h1');
+                                commentUserID.innerText = comment.postId;
+                                let commentId = document.createElement('h3');
+                                commentId.innerText = comment.id;
+                                let commentName = document.createElement('h2');
+                                commentName.innerText = comment.name;
+                                let commentEmail = document.createElement('h2');
+                                commentEmail.innerText = comment.email;
+                                let commentBody = document.createElement('p');
+                                commentBody.innerText = comment.body;
+                                divComment.append(commentUserID, commentId, commentName, commentEmail, commentBody);
+                                rightPart.appendChild(divComment);
+                            }
+                        });
+                    });
+            };
+            divPost.append(postUserID, postId, postTitle, postBody, divPostButton);
+            leftPart.appendChild(divPost);
+        });
     });
