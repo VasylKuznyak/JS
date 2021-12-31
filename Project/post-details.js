f2cc5e65eb006e21b118303399bd654aa9a6c10f
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2021-12-31 02:38:04 +0200

    done project

--- Project/post-details.js
+++ Project/post-details.js
@@ -2,10 +2,9 @@ let postDetails = JSON.parse(localStorage.getItem('post'));
 let divDetail = document.createElement('div');
 divDetail.classList.add('detail');
 divDetail.innerHTML = `
-    <p>User id: ${postDetails.userId}</p>
-    <h2>Id: ${postDetails.id}</h2>
+    <h2>User id: ${postDetails.userId} - Id: ${postDetails.id}</h2>
     <h1>Title: ${postDetails.title}</h1>
-    <h3>Body: ${postDetails.body}</h3>
+    <h3>Post: ${postDetails.body}</h3>
 `;
 document.body.appendChild(divDetail);
 
@@ -13,11 +12,14 @@ let h1 = document.createElement('h1');
 h1.innerText = 'Comments:';
 document.body.appendChild(h1);
 
+let wrapper = document.createElement('wrapper');
+wrapper.classList.add('wrap');
+document.body.appendChild(wrapper);
 fetch('https://jsonplaceholder.typicode.com/comments')
     .then(value => value.json())
     .then(comments => {
         comments.forEach(comment => {
-            if (comment.id === postDetails.id) {
+            if (comment.postId === postDetails.id) {
                 let divComment = document.createElement('div');
                 divComment.classList.add('comment');
                 divComment.innerHTML = `
@@ -26,7 +28,7 @@ fetch('https://jsonplaceholder.typicode.com/comments')
                    <h1>${comment.name}</h1>
                    <h2>${comment.email}</h2>
                    <h3>${comment.body}</h3>`;
-                document.body.appendChild(divComment);
+                wrapper.appendChild(divComment);
             }
         });
     });
--- Project/users.js
+++ Project/users.js
@@ -7,7 +7,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
         users.forEach(user => {
             let userDiv = document.createElement('div');
             userDiv.classList.add('half')
-            userDiv.innerHTML = `${user.id}. ${user.name}`;
+            userDiv.innerHTML = `<h1>${user.id}</h1><h2>${user.name}`;
             let userButton = document.createElement('button');
             userButton.innerText = 'Details';
             userButton.onclick = function () {
